42b3901cc19e58a832d0f19e2b30d06270f9a407
ankit <ankitxrajpoot@gmail.com>
2024-10-05 16:56:26 +0530

    added user login api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -52,4 +52,42 @@ router.post(
     }
   }
 );
+
+// Authenticate a User using: POST "/api/auth/login". 
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password can't be blank").exists(),
+  ],async (req, res) => {
+    // If there are errors, return Bad request and the errors
+     // If there are errors, return Bad request and the errors
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+       return res.status(400).json({ errors: errors.array() });
+     }
+
+     try {
+      let user = await User.findOne({ email: req.body.email });
+      const password = req.body.password;
+      if (!user) {
+        return res.status(400).json({ error: "Invalid credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if(!passwordCompare) {
+        return res.status(400).json({ error: "Incorrect password" });
+      }
+
+      const payload = {
+        user_id: user._id.toString(),
+      }
+      const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h"});
+      res.json(authtoken);
+
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal server Error occured");
+    }
+  });
 export default router;
