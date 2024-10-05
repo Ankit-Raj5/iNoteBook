716c9dd7cbd4de7f23827c3837d3cbc86f26039b
ankit <ankitxrajpoot@gmail.com>
2024-10-05 16:27:28 +0530

    password security and jwt token created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from "../models/User.js";
 
+const JWT_SECRET = 'Innooooo';
 const router = express.Router();
 
 // Create a User using: POST "/api/auth/createuser". No login required
@@ -30,12 +33,19 @@ router.post(
           .json({ error: "Sorry a user with this email already exists" });
       }
       // Create a new user
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
-      res.json(user);
+      const authtoken = jwt.sign(
+        { user_id: user._id.toString() },
+        JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.json(authtoken);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error occured");
